7310bd9856301f55b8c2d12c5d087a8047870a17
Eugene Oparyshev <eop@sitecore.net>
2017-04-06 11:28:47 +0300

    Patch commit

---
+++ src/Sitecore.Support.156023/ExperienceEditor/Speak/Ribbon/Requests/Insert/CanInsertRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.Insert
+{
+    public class CanInsertRequest : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.Insert.CanInsertRequest
+    {
+        public override bool GetControlState()
+        {
+            base.RequestContext.ValidateContextItem();
+            return !(base.RequestContext.WebEditMode != "edit") && base.RequestContext.Item.Access.CanCreate() && base.RequestContext.Item.Access.CanWriteLanguage();
+        }
+    }
+}
---
+++ src/Sitecore.Support.156023/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.156023")]
+[assembly: AssemblyProduct("Sitecore.Support.156023")]
+[assembly: ComVisible(false)]
